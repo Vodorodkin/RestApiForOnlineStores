d5c6989154641af80293778b40347cc5367280e6
d.vodorodkin <vodorodkin@gmail.com>
2022-02-04 01:21:44 +0300

    - adding FormatValidator.cs;

---
+++ RestApiForOnlineStores.Tools/FormatValidators/FormatValidator.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+using Jane;
+
+namespace RestApiForOnlineStores.Tools.FormatValidators
+{
+    public static class FormatValidator
+    {
+        private static readonly Regex FormatPhoneNumberRus = new ("/+7[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}",RegexOptions.Compiled);
+        private static readonly Regex FormatPostamatId = new ("[0-9]{3}-[0-9]{3}",RegexOptions.Compiled);
+
+        public static IResult IsCorrectPhoneNumber(string phoneNumber)
+        {
+            return FormatPhoneNumberRus.IsMatch(phoneNumber)
+                ? Result.Success()
+                : Result.Failure("Неверный формат номера телефона");
+        }
+
+        public static IResult IsCorrectPostamatId(string postamatId)
+        {
+            return FormatPostamatId.IsMatch(postamatId)
+                ? Result.Success()
+                : Result.Failure("Неверный формат номера постамата");
+        }
+    }
+}
