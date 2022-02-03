d6c837ffd93a3b2176da9cd73b4cccb283b5666e
d.vodorodkin <vodorodkin@gmail.com>
2022-02-03 22:50:48 +0300

    - adding tools project; - adding EnumExtensions;

---
+++ RestApiForOnlineStores.Tools/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace RestApiForOnlineStores.Tools.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue) 
+            where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<TAttribute>();
+        }
+        
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetAttribute<DisplayAttribute>()?.Name;
+        }
+    }
+}
