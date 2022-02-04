c3d13c0de0863311c698449671e1d077bf49d82c
d.vodorodkin <vodorodkin@gmail.com>
2022-02-04 03:47:09 +0300

    - adding methods to OrdersController.cs;

--- RestApiForOnlineStores.API/Controllers/OrdersController.cs
+++ RestApiForOnlineStores.API/Controllers/OrdersController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Jane;
+using Microsoft.AspNetCore.Mvc;
+using RestApiForOnlineStores.Domain.Orders.Models;
 using RestApiForOnlineStores.Domain.Orders.Services.Interfaces;
 
 namespace RestApiForOnlineStores.API.Controllers
 {
+    [Route("/Orders")]
     public class OrdersController : Controller
     {
         private readonly IOrdersService _ordersService;
@@ -10,6 +15,70 @@ namespace RestApiForOnlineStores.API.Controllers
         public OrdersController(IOrdersService ordersService)
         {
             _ordersService = ordersService;
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult> CreateOrderAsync(OrderBlank orderBlank)
+        {
+            try
+            {
+                IResult createOrderResult = await _ordersService.CreateOrderAsync(orderBlank);
+
+                return new JsonResult(createOrderResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        [HttpPost("Edit")]
+        public async Task<ActionResult> EditOrderAsync(OrderBlank orderBlank)
+        {
+            try
+            {
+                IResult editOrderResult = await _ordersService.EditOrderAsync(orderBlank);
+
+                return new JsonResult(editOrderResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [HttpGet("GetById")]
+        public async Task<ActionResult> GetOrderByIdAsync(int? orderId)
+        {
+            try
+            {
+                IResult getOrderByIdResult = await _ordersService.GetOrderByIdAsync(orderId);
+
+                return new JsonResult(getOrderByIdResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        [HttpPost("Cancel")]
+        public async Task<ActionResult> CancelOrderAsync([FromBody]int? orderId)
+        {
+            try
+            {
+                IResult cancelOrderResult = await _ordersService.CancelOrderAsync(orderId);
+
+                return new JsonResult(cancelOrderResult);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
