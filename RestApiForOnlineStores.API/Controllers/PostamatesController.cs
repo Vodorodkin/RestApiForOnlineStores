4455082b0549f5ca398460b91b9c04db2be11992
d.vodorodkin <vodorodkin@gmail.com>
2022-02-04 06:12:25 +0300

    - adding methods in PostamatesController.cs;

--- RestApiForOnlineStores.API/Controllers/PostamatesController.cs
+++ RestApiForOnlineStores.API/Controllers/PostamatesController.cs
@@ -1,8 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jane;
+using Microsoft.AspNetCore.Mvc;
+using RestApiForOnlineStores.Domain.Postamates.Converters;
+using RestApiForOnlineStores.Domain.Postamates.Models;
 using RestApiForOnlineStores.Domain.Postamates.Services.Interfaces;
 
 namespace RestApiForOnlineStores.API.Controllers
 {
+    [Route("Postamates")]
     public class PostamatesController : Controller
     {
         private readonly IPostamatesService _postamatesService;
@@ -12,6 +19,56 @@ namespace RestApiForOnlineStores.API.Controllers
             _postamatesService = postamatesService;
         }
         
+        [HttpGet("GetActive")]
+        public async Task<ActionResult> GetActivePostamatesAsync()
+        {
+            try
+            {
+                IResult<IEnumerable<Postamat>> getOrderByIdResult = await _postamatesService.GetActivePostamates();
+                JsonResult jsonResult;
+                
+                if (getOrderByIdResult.Ok)
+                {
+                    jsonResult = new JsonResult(Result.Success(getOrderByIdResult.Value.ToPostamatViews()));
+                }
+                else
+                {
+                    jsonResult = new JsonResult(Result.Failure(getOrderByIdResult.Reason));
+                }
+                
+                return jsonResult;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
         
+        [HttpGet("GetById")]
+        public async Task<ActionResult> GetPostamatByIdAsync([FromQuery]string postamatId)
+        {
+            try
+            {
+                IResult<Postamat> getOrderByIdResult = await _postamatesService.GetPostamatById(postamatId);
+                JsonResult jsonResult;
+                
+                if (getOrderByIdResult.Ok)
+                {
+                    jsonResult = new JsonResult(Result.Success(getOrderByIdResult.Value.ToPostamatView()));
+                }
+                else
+                {
+                    jsonResult = new JsonResult(Result.Failure(getOrderByIdResult.Reason));
+                }
+                
+                return jsonResult;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
     }
 }
